32d2af4a8762e79ff86cbb7ba4a07fb67fb9be9d
Charlotte Pak <charlottepak@outlook.com>
2020-07-15 16:52:55 -0700

    render questions

---
+++ src/api/quizQuestions.jsx
@@ -0,0 +1,63 @@
+var quizQuestions = [
+    {
+        question: "What do you spend your paycheck on?",
+        answers: [
+            {
+                type: "Ron",
+                content: "Food and stuff"
+            },
+            {
+                type: "Donna" + "Tom",
+                content: "Treating yo self"
+            },
+            {
+                type: "Leslie" + "Ann",
+                content: "Friends and family"
+            },
+            {
+                type: "Ben",
+                content: "Unfair rent"
+            },
+            {
+                type: "April" + "Andy",
+                content: "'As Seen on TV'"
+            },
+            {
+                type: "Jerry" + "Chris",
+                content: "Investments"
+            }
+
+        ]
+    },
+    {
+        question: "What's your favorite food?",
+        answers: [
+            {
+                type: "Leslie",
+                content: "Waffles"
+            },
+            {
+                type: "Ron" + "Tom" + "Donna",
+                content: "Steak"
+            },
+            {
+                type: "Jerry" + "April",
+                content: "Anything Sweetums"
+            },
+            {
+                type: "Chris" + "Ann",
+                content: "Fresh lettuce"
+            },
+            {
+                type: "Andy",
+                content: "Candy"
+            },
+            {
+                type: "Calzones",
+                content: "Ben"
+            }
+        ]
+    },
+  ];
+  
+  export default quizQuestions;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -6,17 +6,108 @@ import NavBar from '../../components/NavBar/NavBar';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import SignupPage from '../SignupPage/SignupPage';
 import LoginPage from '../LoginPage/LoginPage';
+import Question from '../../components/Question/Question';
+import quizQuestions from '../../api/quizQuestions';
+import Quiz from '../../components/Quiz/Quiz';
+import Result from '../../components/Result/Result';
+
 
 
 class App extends Component {
+  
   /*--- State ---*/
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      // Initialize user if there's a token, otherwise null
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {},
+      result: '',
+
       user: userService.getUser()
     };
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
   }
+  componentDidMount() {
+    const shuffledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));  
+  
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0]
+    });
+  }
+
+  shuffleArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    
+    return array;
+  };
+  
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+    if (this.state.questionId < quizQuestions.length) {
+        setTimeout(() => this.setNextQuestion(), 300);
+      } else {
+        setTimeout(() => this.setResults(this.getResults()), 300);
+      }
+  }  
+
+  setUserAnswer(answer) {
+    this.setState((state) => ({
+      answersCount: {
+        ...state.answersCount,
+        [answer]: (state.answersCount[answer] || 0) + 1
+      },
+      answer: answer
+    }));
+  }
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: ''
+    });
+  }
+
+  getResults() {
+    const answersCount = this.state.answersCount;
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map(key => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+
+    return answersCountKeys.filter(key => answersCount[key] === maxAnswerCount);
+  }
+
+  setResults(result) {
+    if (result.length === 1) {
+      this.setState({ result: result[0] });
+    } else {
+      this.setState({ result: result[Math.floor(Math.random() * result.length)] });
+    }
+  }
+
+
     handleLogout = () => {
       userService.logout();
       this.setState({user: null});
@@ -26,12 +117,32 @@ class App extends Component {
       this.setState({user: userService.getUser()});
     }
 
-  /*--- Handle Methods ---*/
-  /*--- Lifecycle Methods ---*/
+    renderQuiz() {
+      return (
+        <Quiz
+          answer={this.state.answer}
+          answerOptions={this.state.answerOptions}
+          questionId={this.state.questionId}
+          question={this.state.question}
+          questionTotal={quizQuestions.length}
+          onAnswerSelected={this.handleAnswerSelected}
+        />
+      );
+    }
+
+    renderResult() {
+      return (
+        <Result quizResult={this.state.result} />
+      );
+    }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header"></header>
+        <div className="App-header">
+          <h2>Character Quiz</h2>
+        </div>
+        {this.state.result ? this.renderResult() : this.renderQuiz()}
         <NavBar
           user={this.state.user}
           handleLogout={this.handleLogout}
@@ -53,10 +164,6 @@ class App extends Component {
       </div>
     );
   }
-
-
-
-
 
 
 }
